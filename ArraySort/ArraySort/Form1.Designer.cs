3ee92c0a4898d3e80ad05de2ae26c525aa5540aa
Samantha15perez <Samantha15perez@gmail.com>
2018-05-01 21:53:07 -0400

    death

---
+++ ArraySort/ArraySort/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ArraySort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxNames = new System.Windows.Forms.ListBox();
+            this.labelOutputTimer = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonOutput = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxNames
+            // 
+            this.listBoxNames.FormattingEnabled = true;
+            this.listBoxNames.Location = new System.Drawing.Point(16, 11);
+            this.listBoxNames.Name = "listBoxNames";
+            this.listBoxNames.Size = new System.Drawing.Size(170, 225);
+            this.listBoxNames.TabIndex = 0;
+            // 
+            // labelOutputTimer
+            // 
+            this.labelOutputTimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelOutputTimer.Location = new System.Drawing.Point(215, 136);
+            this.labelOutputTimer.Name = "labelOutputTimer";
+            this.labelOutputTimer.Size = new System.Drawing.Size(132, 100);
+            this.labelOutputTimer.TabIndex = 1;
+            this.labelOutputTimer.Text = "label1";
+            this.labelOutputTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(215, 12);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(137, 20);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // buttonOutput
+            // 
+            this.buttonOutput.Location = new System.Drawing.Point(250, 100);
+            this.buttonOutput.Name = "buttonOutput";
+            this.buttonOutput.Size = new System.Drawing.Size(62, 23);
+            this.buttonOutput.TabIndex = 4;
+            this.buttonOutput.Text = "Output";
+            this.buttonOutput.UseVisualStyleBackColor = true;
+            this.buttonOutput.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(250, 42);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(62, 23);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click_1);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(250, 71);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(62, 23);
+            this.buttonSort.TabIndex = 6;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 252);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonOutput);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.labelOutputTimer);
+            this.Controls.Add(this.listBoxNames);
+            this.Name = "Form1";
+            this.Text = "NameSort";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxNames;
+        private System.Windows.Forms.Label labelOutputTimer;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonOutput;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSort;
+    }
+}
+
---
+++ ArraySort/ArraySort/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArraySort
+{
+    public partial class Form1 : Form
+    {
+        //creating the array at class level allows for it to be referenced from all parts of the program
+        string[] NameSortArray = new string[5000];
+        int i = 0;
+        StreamReader SR = File.OpenText("Names.Csv");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Swap (ref string a, ref string b)
+        {
+
+            string temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                for (i = 0; i < 5000; i++)
+                {
+                    NameSortArray[i] = SR.ReadLine();
+                    listBoxNames.Items.Add(NameSortArray[i]);
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("E R R O R");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+
+
+                StreamWriter OutputFile = new StreamWriter("SortedNames.CSV");
+                foreach (string name in listBoxNames.Items)
+                {
+                    OutputFile.WriteLine(name.ToString());
+                
+                }
+                MessageBox.Show("Output Complete!");
+            }
+            catch
+            {
+                MessageBox.Show("Output Not Completed");   
+            }
+        }
+
+     
+
+     
+
+        private void SelectionSort(string[] NameSortArray)
+        {
+            int minIndex; //Subscript of smallest value in scanned area
+            string minValue; //smallest value in the scanned area
+
+            //The outter loop steps through all the array elements, Except the last one
+            // The startscan variable marks the position where the scan should begin
+            for (int startScan = 0; startScan < NameSortArray.Length - 1; startScan++)
+            {
+                //Assume the first element in the scannable area 
+                //Is the smallest value
+                minIndex = startScan;
+                minValue = NameSortArray[startScan];
+
+                //Scan the array, starting at the 2nd element in the 
+                //Scannable area, looking for the smallest value.
+                for (int index = startScan + 1; index < NameSortArray.Length; index++)
+                {
+                    if (string.Compare(minValue, NameSortArray[index], true) == 1)
+                    {
+                        minValue = NameSortArray[index];
+                        minIndex = index;
+                    }
+                }
+
+                //Swap the element with the smallest value with the
+                // first element in the scannable area
+                Swap(ref NameSortArray[minIndex], ref NameSortArray[startScan]);
+
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            
+                int counter = 0;
+                DateTime Time1 = DateTime.Now;
+                try
+                {
+                    // Array variables
+                    const int SIZE = 5000;
+                    string[] NameSortArray = new string[SIZE];
+                    StreamReader inputFile = File.OpenText("Names.csv");
+
+
+                    // Input names into Names Array while counter is less than the number of items
+                    while (counter < NameSortArray.Length && !inputFile.EndOfStream)
+                    {
+                        NameSortArray[counter] = inputFile.ReadLine();
+                        counter++;
+                    }
+
+                    // Sort Array alphabetically 
+                    SelectionSort(NameSortArray);
+
+                    listBoxNames.Items.Clear();
+
+                    for (i = 0; i < 5000; i++)
+                    {
+                       
+                        listBoxNames.Items.Add(NameSortArray[i]);
+                    }
+
+                    Double Time2 = (DateTime.Now - Time1).TotalSeconds;
+                    labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+
+
+
+                     
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+        }
+
+        private int BinarySearch(string[] NameSortArray, string value)
+        {
+            int first = 0;
+            int last = NameSortArray.Length - 1;
+            int middle;
+            int position = -1;
+            bool found = false;
+
+            while (!found && first <= last)
+            {
+                middle = (first + last) / 2;
+                if (NameSortArray[middle] == value)
+                {
+                    found = true;
+                    position = middle;
+                }
+                else if (string.Compare(NameSortArray[middle], value, false) > 0)
+                {
+                    last = middle - 1;
+                }
+                else
+                {
+                    first = middle + 1;
+                }
+
+            }
+           
+            return position;
+
+        }
+
+        private void buttonSearch_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+               string SearchBox = textBoxSearch.Text;
+                int Position = BinarySearch(NameSortArray, textBoxSearch.Text);
+bool SearchFound = NameSortArray.Contains(SearchBox);
+                
+
+                if (SearchFound == false)
+                {
+                    MessageBox.Show("The name you searched could not be found.");
+                }
+                else
+                {
+                    MessageBox.Show(Position.ToString());
+                }
+            }
+            catch
+            {
+                MessageBox.Show("eeee");
+            }
+        }
+    }
+}
