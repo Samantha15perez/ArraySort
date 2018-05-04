085e44421588e4dc80285cc895f0c6ef02cdb293
Samantha15perez <Samantha15perez@gmail.com>
2018-05-03 21:26:04 -0400

    fine tuning finished

--- ArraySort/ArraySort/Form1.Designer.cs
+++ ArraySort/ArraySort/Form1.Designer.cs
@@ -42,7 +42,7 @@
             // listBoxNames
             // 
             this.listBoxNames.FormattingEnabled = true;
-            this.listBoxNames.Location = new System.Drawing.Point(12, 12);
+            this.listBoxNames.Location = new System.Drawing.Point(11, 11);
             this.listBoxNames.Name = "listBoxNames";
             this.listBoxNames.Size = new System.Drawing.Size(150, 225);
             this.listBoxNames.TabIndex = 0;
@@ -51,7 +51,7 @@
             // 
             this.labelOutputTimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.labelOutputTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.labelOutputTimer.Location = new System.Drawing.Point(189, 152);
+            this.labelOutputTimer.Location = new System.Drawing.Point(188, 151);
             this.labelOutputTimer.Name = "labelOutputTimer";
             this.labelOutputTimer.Size = new System.Drawing.Size(108, 85);
             this.labelOutputTimer.TabIndex = 1;
@@ -59,82 +59,94 @@
             // 
             // textBoxSearch
             // 
-            this.textBoxSearch.Location = new System.Drawing.Point(175, 42);
+            this.textBoxSearch.Location = new System.Drawing.Point(174, 41);
             this.textBoxSearch.Name = "textBoxSearch";
             this.textBoxSearch.Size = new System.Drawing.Size(137, 20);
             this.textBoxSearch.TabIndex = 3;
             // 
             // buttonOutput
             // 
-            this.buttonOutput.Location = new System.Drawing.Point(250, 13);
+            this.buttonOutput.Location = new System.Drawing.Point(249, 12);
             this.buttonOutput.Name = "buttonOutput";
             this.buttonOutput.Size = new System.Drawing.Size(62, 23);
             this.buttonOutput.TabIndex = 4;
             this.buttonOutput.Text = "Output";
             this.buttonOutput.UseVisualStyleBackColor = true;
             this.buttonOutput.Click += new System.EventHandler(this.button2_Click);
+            this.buttonOutput.MouseLeave += new System.EventHandler(this.buttonOutput_MouseLeave);
+            this.buttonOutput.MouseHover += new System.EventHandler(this.buttonOutput_MouseHover);
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(212, 68);
+            this.buttonSearch.Location = new System.Drawing.Point(211, 67);
             this.buttonSearch.Name = "buttonSearch";
             this.buttonSearch.Size = new System.Drawing.Size(62, 23);
             this.buttonSearch.TabIndex = 5;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
             this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click_1);
+            this.buttonSearch.MouseLeave += new System.EventHandler(this.buttonSearch_MouseLeave);
+            this.buttonSearch.MouseHover += new System.EventHandler(this.buttonSearch_MouseHover);
             // 
             // buttonSort
             // 
-            this.buttonSort.Location = new System.Drawing.Point(175, 13);
+            this.buttonSort.Location = new System.Drawing.Point(174, 12);
             this.buttonSort.Name = "buttonSort";
             this.buttonSort.Size = new System.Drawing.Size(62, 23);
             this.buttonSort.TabIndex = 6;
             this.buttonSort.Text = "Sort";
             this.buttonSort.UseVisualStyleBackColor = true;
             this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            this.buttonSort.MouseLeave += new System.EventHandler(this.buttonSort_MouseLeave);
+            this.buttonSort.MouseHover += new System.EventHandler(this.buttonSort_MouseHover);
             // 
             // radioButtonExact
             // 
             this.radioButtonExact.AutoSize = true;
-            this.radioButtonExact.Location = new System.Drawing.Point(212, 97);
+            this.radioButtonExact.Location = new System.Drawing.Point(191, 91);
             this.radioButtonExact.Name = "radioButtonExact";
-            this.radioButtonExact.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonExact.Size = new System.Drawing.Size(89, 17);
             this.radioButtonExact.TabIndex = 7;
             this.radioButtonExact.TabStop = true;
-            this.radioButtonExact.Text = "Exact";
+            this.radioButtonExact.Text = "Exact Search";
             this.radioButtonExact.UseVisualStyleBackColor = true;
             this.radioButtonExact.CheckedChanged += new System.EventHandler(this.radioButtonExact_CheckedChanged);
+            this.radioButtonExact.MouseLeave += new System.EventHandler(this.radioButtonExact_MouseLeave);
+            this.radioButtonExact.MouseHover += new System.EventHandler(this.radioButtonExact_MouseHover);
             // 
             // radioButtonPartial
             // 
             this.radioButtonPartial.AutoSize = true;
-            this.radioButtonPartial.Location = new System.Drawing.Point(212, 111);
+            this.radioButtonPartial.Location = new System.Drawing.Point(191, 105);
             this.radioButtonPartial.Name = "radioButtonPartial";
-            this.radioButtonPartial.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonPartial.Size = new System.Drawing.Size(91, 17);
             this.radioButtonPartial.TabIndex = 7;
             this.radioButtonPartial.TabStop = true;
-            this.radioButtonPartial.Text = "Partial";
+            this.radioButtonPartial.Text = "Partial Search";
             this.radioButtonPartial.UseVisualStyleBackColor = true;
             this.radioButtonPartial.CheckedChanged += new System.EventHandler(this.radioButtonPartial_CheckedChanged);
+            this.radioButtonPartial.MouseLeave += new System.EventHandler(this.radioButtonPartial_MouseLeave);
+            this.radioButtonPartial.MouseHover += new System.EventHandler(this.radioButtonPartial_MouseHover);
             // 
             // radioButtonStartsWith
             // 
             this.radioButtonStartsWith.AutoSize = true;
-            this.radioButtonStartsWith.Location = new System.Drawing.Point(212, 125);
+            this.radioButtonStartsWith.Location = new System.Drawing.Point(191, 119);
             this.radioButtonStartsWith.Name = "radioButtonStartsWith";
-            this.radioButtonStartsWith.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonStartsWith.Size = new System.Drawing.Size(82, 17);
             this.radioButtonStartsWith.TabIndex = 7;
             this.radioButtonStartsWith.TabStop = true;
-            this.radioButtonStartsWith.Text = "Beginning";
+            this.radioButtonStartsWith.Text = "Begins With";
             this.radioButtonStartsWith.UseVisualStyleBackColor = true;
             this.radioButtonStartsWith.CheckedChanged += new System.EventHandler(this.radioButtonStartsWith_CheckedChanged);
+            this.radioButtonStartsWith.MouseLeave += new System.EventHandler(this.radioButtonStartsWith_MouseLeave);
+            this.radioButtonStartsWith.MouseHover += new System.EventHandler(this.radioButtonStartsWith_MouseHover);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 252);
+            this.ClientSize = new System.Drawing.Size(322, 252);
             this.Controls.Add(this.radioButtonStartsWith);
             this.Controls.Add(this.radioButtonPartial);
             this.Controls.Add(this.radioButtonExact);
--- ArraySort/ArraySort/Form1.cs
+++ ArraySort/ArraySort/Form1.cs
@@ -9,19 +9,31 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
+//This project currently has all 20000 names loaded. in order to change it back to 5000
+//change arrayLength to 5000, and the actual length of the array (in the line above) to 5000
+//then the name of the file you wish to open.
+
 
 namespace ArraySort
 {
+
     public partial class Form1 : Form
     {
+        
+
         //creating the array at class level allows for it to be referenced from all parts of the program
-        string[] NameSortArray = new string[5000];
+        string[] NameSortArray = new string[20000];
+        int ArrayLength = 20000;
         int i = 0;
         bool Exact = false;
         bool Partial = false;
         bool StartsWith = false;
+
+        
+
+        string TempLabel = "";
         //actually reads the specified file
-        StreamReader SR = File.OpenText("Names.Csv");
+        StreamReader SR = File.OpenText("20000Names.csv");
 
         public Form1()
         {
@@ -47,7 +59,7 @@ namespace ArraySort
 
             try
             {
-                for (i = 0; i < 5000; i++)
+                for (i = 0; i < ArrayLength; i++)
                 {
                     //reads every name within the array and inserts them into the listbox
                     NameSortArray[i] = SR.ReadLine();
@@ -80,6 +92,7 @@ namespace ArraySort
                 //subtracts the time from the beginning of the action to get an accurate timestamp
                 Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                 labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                TempLabel = labelOutputTimer.Text.ToString();
                 MessageBox.Show("Output Complete!");
                 
             }
@@ -88,6 +101,7 @@ namespace ArraySort
                 //subtracts the time from the beginning of the action to get an accurate timestamp
                 Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                 labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                TempLabel = labelOutputTimer.Text.ToString();
                 MessageBox.Show(ex.Message);
 
             }
@@ -127,6 +141,8 @@ namespace ArraySort
             }
         }
 
+
+
         private void buttonSort_Click(object sender, EventArgs e)
         {
 
@@ -140,7 +156,7 @@ namespace ArraySort
                     //clears list box
                     listBoxNames.Items.Clear();
 
-                    for (i = 0; i < 5000; i++)
+                    for (i = 0; i < ArrayLength; i++)
                     {
                        //adds all the names back into the listbox in the sorted order
                         listBoxNames.Items.Add(NameSortArray[i]);
@@ -151,6 +167,7 @@ namespace ArraySort
                 Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                 //outputs time taken
                 labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                TempLabel = labelOutputTimer.Text.ToString();
 
                 //once the names have been sorted, the search box, radio buttons, and button become available.
                 buttonSearch.Visible = true;
@@ -169,6 +186,7 @@ namespace ArraySort
                 Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                 //outputs time taken
                 labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                TempLabel = labelOutputTimer.Text.ToString();
                 //error handling
                 MessageBox.Show(ex.Message);
                 
@@ -178,8 +196,7 @@ namespace ArraySort
 
         private int BinarySearch(string[] NameSortArray, string value, bool Exact, bool StartsWith, bool Partial)
         {
-            //binary search method is for exact
-            //i kept the sequential search (for partial and startswith) in the same method for ease of access
+            //binary search method was taken from the textbook and tweaked for use with strings intead of ints.
 
 
             int first = 0;
@@ -215,26 +232,33 @@ namespace ArraySort
 
             if (StartsWith == true)
             {
-                int i = 0;
-
-                while (i < last)
-                {
-                    if (NameSortArray[i].StartsWith(value))
+                //when the StartsWith radio button is selected, it uses the binary search method to find the first value
+                //in the array that starts with the value entered into the textbox.
+                    while (!found && first <= last)
                     {
-                        position = i;
-                        return position;
+                        middle = (first + last) / 2;
+                        if (NameSortArray[middle].StartsWith(value))
+                        {
+                            found = true;
+                            position = middle;
+                        }
+                        else if (string.Compare(NameSortArray[middle], value, false) > 0)
+                        {
+                            last = middle - 1;
+                        }
+                        else
+                        {
+                            first = middle + 1;
+
+                        }
 
                     }
-                    else
-                    {
-                        i++;
-                    }
-                }
+                    return position;
 
 
-            position = -1;
-            return position;
             }
+
+            //the partial search is sequential, sorting through all of the names until a matching name is found.
             if (Partial == true)
             {
                 int i = 0;
@@ -280,7 +304,7 @@ namespace ArraySort
                 int Position = BinarySearch(NameSortArray, textBoxSearch.Text, Exact, StartsWith, Partial);
 
                 if (Position >= 0)
-                {
+                { //if the position is valid, or rather anything other than -1, it changes the boolean to true. 
                     SearchFound = true;
                 }
                 else
@@ -294,6 +318,7 @@ namespace ArraySort
                     //subtracts the time from the beginning of the action to get an accurate timestamp
                     Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                     labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                    TempLabel = labelOutputTimer.Text.ToString();
                     MessageBox.Show("The name you searched could not be found.");
 
                 }
@@ -303,6 +328,7 @@ namespace ArraySort
                     //subtracts the time from the beginning of the action to get an accurate timestamp
                     Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                     labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                    TempLabel = labelOutputTimer.Text.ToString();
 
                     //finds and highlights the correct listbox item corresponding to the name. 
                     listBoxNames.SetSelected(Position, true);
@@ -314,6 +340,7 @@ namespace ArraySort
                 //subtracts the time from the beginning of the action to get an accurate timestamp
                 Double Time2 = (DateTime.Now - Time1).TotalSeconds;
                 labelOutputTimer.Text = ("Time elapsed: " + Time2 + " Seconds.");
+                TempLabel = labelOutputTimer.Text.ToString();
                 MessageBox.Show(ex.Message);
             }
 
@@ -342,5 +369,69 @@ namespace ArraySort
             Exact = false;
             Partial = false;
         }
+
+        //this last chunk of code changes the label text to an explanation of what each control does, then changes it back when the mouse finishes hovering.
+
+        private void buttonSort_MouseHover(object sender, EventArgs e)
+        {
+
+            labelOutputTimer.Text = "Sort the names alphabetically";
+        }
+
+        private void buttonSort_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
+        private void buttonOutput_MouseHover(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = "Output the names in their current order to a CSV file";
+        }
+
+        private void buttonOutput_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
+        private void buttonSearch_MouseHover(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = "Search the list of names for a specific value";
+        }
+
+        private void buttonSearch_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
+        private void radioButtonExact_MouseHover(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = "Search with exact formatting";
+        }
+
+        private void radioButtonExact_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
+        private void radioButtonStartsWith_MouseHover(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = "Search the beginning of a last name";
+        }
+
+        private void radioButtonStartsWith_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
+        private void radioButtonPartial_MouseHover(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = "Search for names that contain your input";
+        }
+
+        private void radioButtonPartial_MouseLeave(object sender, EventArgs e)
+        {
+            labelOutputTimer.Text = TempLabel;
+        }
+
     }
 }
